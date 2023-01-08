9af9824c031ec442ed8bab37c113dee59aa4a512
James <wjamesfl@gmail.com>
2023-01-08 18:55:09 -0500

    GWT fixes

--- core/src/com/jamestkhan/voxels/voxels/PerlinNoiseGenerator.java
+++ core/src/com/jamestkhan/voxels/voxels/PerlinNoiseGenerator.java
@@ -117,16 +117,18 @@ public class PerlinNoiseGenerator {
 	}
 
 	public static Pixmap generatePixmap (int width, int height, int min, int max, int octaveCount) {
-		byte[] bytes = generateHeightMap(width, height, min, max, octaveCount);
-		Pixmap pixmap = new Pixmap(width, height, Format.RGBA8888);
-		for (int i = 0, idx = 0; i < bytes.length; i++) {
-			byte val = bytes[i];
-			pixmap.getPixels().put(idx++, val);
-			pixmap.getPixels().put(idx++, val);
-			pixmap.getPixels().put(idx++, val);
-			pixmap.getPixels().put(idx++, (byte)255);
-		}
-		return pixmap;
+		// Commented out, not compatible with GWT
+//		byte[] bytes = generateHeightMap(width, height, min, max, octaveCount);
+//		Pixmap pixmap = new Pixmap(width, height, Format.RGBA8888);
+//		for (int i = 0, idx = 0; i < bytes.length; i++) {
+//			byte val = bytes[i];
+//			pixmap.getPixels().put(idx++, val);
+//			pixmap.getPixels().put(idx++, val);
+//			pixmap.getPixels().put(idx++, val);
+//			pixmap.getPixels().put(idx++, (byte)255);
+//		}
+//		return pixmap;
+		return null;
 	}
 
 	public static void generateVoxels (VoxelWorld voxelWorld, int min, int max, int octaveCount) {
